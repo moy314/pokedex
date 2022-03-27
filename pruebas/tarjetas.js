50de45c0d4283896c0a6fe41858aa6ec9ba317d8
Moises Sanchez <moy5921@gmail.com>
2022-03-27 16:01:59 -0600

    agregando tarjetas

---
+++ pruebas/tarjetas.js
@@ -0,0 +1,102 @@
+const container = document.querySelector(".container");
+
+function fetchPokemon(id){
+
+    
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data);
+     createPokemon(data);
+
+    })
+
+
+    
+}
+
+
+function numeroPokemons(num){
+
+    for(i = 1; i<= num;i++){
+
+
+        fetchPokemon(i);
+    }
+}
+
+
+
+//fetchPokemon(1);
+
+
+
+function createPokemon(pokemon){
+
+    const card = document.createElement('div');
+    card.classList.add('poke-block');
+    
+
+
+    const spriteContainer = document.createElement('div');
+    spriteContainer.classList.add('imgContainer');
+
+    const sprite = document.createElement('img');
+    sprite.src = pokemon.sprites.front_default;
+
+    spriteContainer.appendChild(sprite);
+
+    const number = document.createElement('p');
+    number.textContent = `#${pokemon.id.toString().padStart(3,0)}`;
+
+    const name = document.createElement('p');
+    name.classList.add('name');
+    name.textContent = pokemon.name;
+
+    const type = document.createElement('p');
+    type.classList.add('color');
+    type.textContent = pokemon.types[0].type.name;
+
+    card.appendChild(number);
+    card.appendChild(spriteContainer);
+    card.appendChild(name);
+    card.appendChild(type);
+    container.appendChild(card);
+    
+    
+    
+}
+
+
+
+
+
+
+
+numeroPokemons(9);
+
+
+
+//--------------colores-----
+
+const typeColors = {
+    electric: '#FFEA70',
+    normal: '#B09398',
+    fire: '#FF675C',
+    water: '#0596C7',
+    ice: '#AFEAFD',
+    rock: '#999799',
+    flying: '#7AE7C7',
+    grass: '#4A9681',
+    psychic: '#FFC6D9',
+    ghost: '#561D25',
+    bug: '#A2FAA3',
+    poison: '#795663',
+    ground: '#D2B074',
+    dragon: '#DA627D',
+    steel: '#1D8A99',
+    fighting: '#2F2F2F',
+    default: '#2A1A1F'
+}
+
+console.log(typeColors["ice"]);
